68fa709309b7348e71ce2bcb8fba164691c94364
billyang <billyang3416@gamil.com>
2020-05-05 10:58:22 +0800

    任務02-2&任務03:模板切版及元件規劃，新增articles, tags, footer。另外內嵌繫結title&subtitle

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { ArticlesComponent } from './articles/articles.component';
+import { TagsComponent } from './tags/tags.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    ArticlesComponent,
+    TagsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
